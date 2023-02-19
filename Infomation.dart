98fbe3ac3eb4b1ca089fa9be78c4d61c3548a0d0
Edmund Quainoo <edmundquainoo3018@gmail.com>
2023-02-19 18:59:47 +0000

    Make sure you read this info

---
+++ Infomation.dart
@@ -0,0 +1,14 @@
+void main() {
+  print(
+      "I am collecting my assignment tonight\n I want you guys to push all 10 assignments to your branch that I will merge all  ");
+
+  print(
+      "I am expecting everyone to push your codes to thge various branches latest by 11:59... ThANK yOu");
+
+  print(kwofie);
+}
+
+ kwofie() {
+  print("Hello World");
+ 
+}
