49b71f35844d31a4a61ad1279a692522e14231b9
NA-spendy <nanaamoah2017@gmail.com>
2023-02-20 00:01:09 +0000

    assignment

---
+++ Assignment 5.dart
@@ -0,0 +1,12 @@
+//Question 5
+void main() {
+  //enter first number
+  var first_number = 5;
+  //enter second number
+  var second_number = 15;
+
+  var quotient = first_number ~/ second_number;
+  var remainder = first_number % second_number;
+  print(quotient);
+  print(remainder);
+}
---
+++ Assignment 9.dart
@@ -0,0 +1,11 @@
+void main() {
+  //enter the bill
+  var bill = 250;
+  //enter the num of friends
+  var num_friends = 3;
+//everyone will pay part
+var pay = bill ~/ num_friends;
+print("Bill is equal to $bill");
+print ("Number of friensds are $num_friends");
+print ("Everyone will pay $pay");
+}
