af210458cfbfded360a662d32b35be2ec27b1b59
Humu1 <125820207+Humu1@users.noreply.github.com>
2023-02-20 05:49:20 -0800

    YoU MADE A MISTake

--- Infomation.dart
+++ Infomation.dart
@@ -3,7 +3,7 @@ void main() {
       "I am collecting my assignment tonight\n I want you guys to push all 10 assignments to your branch that I will merge all  ");
 
   print(
-      "I am expecting everyone to push your codes to thge various branches latest by 11:59... ThANK yOu");
+      "I am expecting everyone to push your codes to the various branches latest by 11:59pm... ThANK yOu");
 
   print(kwofie);
 }
