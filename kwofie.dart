19b6203bc47d4e6c5de8e1fa018254a6e035be7f
Sammy <samfie123@outlook.com>
2023-02-21 17:07:57 +0000

    done

---
+++ kwofie.dart
@@ -0,0 +1,28 @@
+void main(){
+ // Functions
+ var_myName("Samuel Kwofie");
+ print('My name is $myName');
+
+ string("John Doe");
+ print('Hello I am $string');
+
+ string1("John Doe");
+ print('Hello iam $string1');
+
+ int("simple_interest"){
+      p*r*t/100;
+      print*("simple_interest");
+ }
+
+int("var a"){
+  return (a^2);
+  print("var a");
+}
+
+
+
+
+}
+
+
+
