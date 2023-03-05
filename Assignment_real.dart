2259e8f9b9ca17674860ab0dbf7e37fd30eb88f1
BillyRyan123 <samfie123@outlook.com>
2023-03-05 16:16:35 +0000

    done

---
+++ Assignment_real.dart
@@ -0,0 +1,26 @@
+ void main(List<String> args) {
+  String word= "Your name";
+  print(word);
+}
+
+void main() {
+  String name ="John Doe";
+  String namey ="John Doe";
+  print("Hello I'm "name"');
+  print('Hello Iam "namey"');
+}
+
+void si() {
+  var p = 1000;
+  var t = 2;
+  var r = 5;
+  var si = (p * t * r) / 100;
+  print('Simple interest = $si');
+}
+
+void main() {
+  print('Enter a number:');
+  var num = int.parse(stdin.readLineSync()!);
+  var square = num * num;
+  print('Square of $num is $square');
+}
